b220474f3922ec355a199cb91296c5de8168512b
Dinshaw <gobhai@gmai.com>
2018-03-12 21:02:19 -0400

    Add specs

---
+++ spec/features/cms_image_spec.rb
@@ -0,0 +1,21 @@
+RSpec.feature 'As a admin' do
+  let(:user) { users(:admin) }
+  let(:title) { FFaker::Lorem.sentence }
+  let(:description) { FFaker::HipsterIpsum.paragraph }
+
+  before do
+    login user
+    visit admin_cms_images_path
+  end
+
+  scenario 'I can create CmsImages' do
+    click_on 'New Cms Image'
+    fill_in 'Title', with: title
+    fill_in 'Description', with: description
+    attach_file 'Image', Rails.root.join('spec/fixtures/logo.png')
+    click_on 'Create Cms image'
+    expect(page).to have_content title
+    expect(page).to have_content description
+    expect(page).to have_css("img[src*='logo.png']")
+  end
+end
---
+++ spec/features/cms_pages_spec.rb
@@ -0,0 +1,30 @@
+RSpec.feature 'As an admin user' do
+  let(:user) { users(:admin) }
+  let(:title) { FFaker::Lorem.sentence }
+  let(:sub_title) { FFaker::HipsterIpsum.sentence }
+  let(:cms_page) { cms_pages(:about_us) }
+
+  before do
+    login user
+    visit admin_cms_pages_path
+  end
+
+  scenario 'I can manage Cms Pages' do
+    click_on 'New Cms Page'
+    fill_in 'Title', with: title
+    fill_in 'Sub title', with: sub_title
+    click_on 'Create Cms page'
+    expect(page).to have_content title
+    expect(page).to have_content sub_title
+  end
+
+  scenario 'I see Cms Page info' do
+    within "#cms_page_#{cms_page.id}" do
+      click_on 'View'
+    end
+    expect(page).to have_content cms_page.title
+    expect(page).to have_content cms_page.sub_title
+    expect(page).to have_css "h1", text: 'Headline'
+    expect(page).to have_content "Link #{cms_page.slug}"
+  end
+end
