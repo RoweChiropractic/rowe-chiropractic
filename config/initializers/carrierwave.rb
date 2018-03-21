6b2a29b7c3df52c125123fccfbbcc69964735c58
Dinshaw <gobhai@gmai.com>
2018-03-20 22:22:41 -0400

    Fog config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,13 @@ CarrierWave.configure do |config|
 
   if Rails.env.production?
     config.fog_provider = 'fog/aws'
-    config.storage = :fog
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     Rails.application.credentials.aws_access_key_id,
       aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
       region:                'us-east-2',
     }
+    config.storage = :fog
     config.fog_directory  = 'rowechiropractic.com'
   end
 end
