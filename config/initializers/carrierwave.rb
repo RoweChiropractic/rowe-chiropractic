cce840f3ec82700b9c0b1f3c89bc6cfd3995e160
Dinshaw <gobhai@gmai.com>
2018-03-20 20:55:09 -0400

    Port to CarrierWave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,22 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     Rails.application.credentials.aws_access_key_id,
+    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+    region:                'us-east-2',
+  }
+  config.fog_directory  = 'rowechiropractic.com'
+
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
+
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+end
