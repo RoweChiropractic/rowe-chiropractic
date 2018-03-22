ef58621b422c43f1504e7dcc29e9fd0f56c570f1
Dinshaw <gobhai@gmai.com>
2018-03-22 00:01:04 -0400

    Add documents
    * Adjust Carrierwave settings

---
+++ app/admin/documents.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Document do
+  controller do
+    def create
+      super do |format|
+        redirect_to collection_url and return if resource.valid?
+      end
+    end
+
+    def update
+      super do |format|
+        redirect_to collection_url and return if resource.valid?
+      end
+    end
+  end
+
+  permit_params do
+    [:title, :description, :file]
+  end
+
+  index do
+    column :title
+    column :file do |document|
+      link_to document.file.url
+    end
+    actions
+  end
+end
