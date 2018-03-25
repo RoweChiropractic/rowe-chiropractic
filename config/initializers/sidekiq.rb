28f9810e5521c81fb0a22afdb66284f7ccf22110
Dinshaw <gobhai@gmai.com>
2018-03-25 14:45:06 -0400

    Remove sidekiq-cron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,5 @@
 Sidekiq.configure_server do |config|
-  schedule_file = 'config/schedule.yml'
-
   config.redis = { url: ENV['REDISTOGO_URL'] }
-
-  if File.exist?(schedule_file)
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
 end
 
 Sidekiq.configure_client do |config|
