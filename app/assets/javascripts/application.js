aa70851c46af75817fe7728a05106e62865c289e
Dinshaw <gobhai@gmai.com>
2019-02-02 17:27:12 +0530

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require rails-ujs
-//= require turbolinks
 //= require jquery3
-//= require jquery.parallax
 //= require popper
 //= require bootstrap-sprockets
 //= require fontawesome-all
+//= require smooth-scroll
+
